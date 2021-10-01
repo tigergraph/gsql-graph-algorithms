6486ac196f82de944fe23bb3cb9723aaadabc90c
Karim Saraipour <karimsaraipour@users.noreply.github.com>
2021-09-30 22:25:30 -0700

    chore: add small syntatical changes to ANN

--- algorithms/Classification/approximate_nearest_neighbors/tg_update_count.hpp
+++ algorithms/Classification/approximate_nearest_neighbors/tg_update_count.hpp
@@ -2,12 +2,16 @@
 #include <algorithm>
 
 
-bool updateCount(const HeapAccum& heap, double incoming_similarity) {
-  std::vector<double> scores;
-  for (auto a : heap.data_)
-    scores.push_back(a.similarity);
-  scores.push_back(incoming_similarity);
-  std::make_heap(std::begin(scores), std::end(scores));
-  std::sort_heap(std::begin(scores), std::end(scores));
-  return (incoming_similarity == scores[0]) ? 0 : 1;
-}
+  bool updateCount(const HeapAccum& heap, double incoming_similarity, int k) {
+    std::vector<double> scores;
+    for (auto a : heap.data_)
+      scores.push_back(a.similarity);
+    
+    if (scores.size() < k)
+      return 1;
+    
+    scores.push_back(incoming_similarity);
+    std::make_heap(std::begin(scores), std::end(scores));
+    std::sort_heap(std::begin(scores), std::end(scores));
+    return (incoming_similarity == scores[0]) ? 0 : 1;
+  }
