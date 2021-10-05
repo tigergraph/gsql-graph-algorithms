62a9c895def5be4a15d41484914054982e55f9a1
wyatt-joyner-tg <82000064+wyatt-joyner-tg@users.noreply.github.com>
2021-10-05 14:50:59 -0400

    perf(Euclidean Similarity): update vector params to pass by ref

--- algorithms/Similarity/euclidean/tg_euclidean_distance.hpp
+++ algorithms/Similarity/euclidean/tg_euclidean_distance.hpp
@@ -10,7 +10,7 @@
 typedef std::string string;
 
 template <typename T>
-inline double tg_euclidean_distance(std::vector<T> A, std::vector<T> B) {
+inline double tg_euclidean_distance(std::vector<T>& A, std::vector<T>& B) {
   int n = A.size();
   double euclidean_distance = 0.0;
   for (int i = 0; i < n; i++)
