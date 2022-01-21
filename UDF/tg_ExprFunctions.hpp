a293b6c3c34ed82751bd16a02c39bc5522e402ab
wyatt-joyner-tg <82000064+wyatt-joyner-tg@users.noreply.github.com>
2022-01-21 09:11:49 +0000

    fix(udf): fix namespace; fix header guard; remove redundant built-ins

--- UDF/tg_ExprFunctions.hpp
+++ UDF/tg_ExprFunctions.hpp
@@ -29,8 +29,8 @@
  *
  ******************************************************************************/
 
-#ifndef EXPRFUNCTIONS_HPP_
-#define EXPRFUNCTIONS_HPP_
+#ifndef TG_EXPRFUNCTIONS_HPP_
+#define TG_EXPRFUNCTIONS_HPP_
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -54,24 +54,8 @@
  */
 #include "tg_ExprUtil.hpp"
 
-namespace tg_UDIMPL {
+namespace UDIMPL {
   typedef std::string string; //XXX DON'T REMOVE
-
-  /****** BIULT-IN FUNCTIONS **************/
-  /****** XXX DON'T REMOVE ****************/
-  inline int str_to_int (string str) {
-    return atoi(str.c_str());
-  }
-
-  inline int float_to_int (float val) {
-    return (int) val;
-  }
-
-  inline string to_string (double val) {
-    char result[200];
-    sprintf(result, "%g", val);
-    return string(result);
-  }
 
   /* ============== START SPEAKER LISTENER LABEL PROP ================== */
 
