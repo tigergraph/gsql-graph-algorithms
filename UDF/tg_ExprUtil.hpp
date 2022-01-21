a53c389220a7de0b53a2cd76dfbf9009fd0f3cac
wyatt-joyner-tg <82000064+wyatt-joyner-tg@users.noreply.github.com>
2022-01-21 16:20:09 +0000

    fix(udf): fix prefixes in ExprUtil

--- UDF/tg_ExprUtil.hpp
+++ UDF/tg_ExprUtil.hpp
@@ -4,21 +4,21 @@
  * Project: TigerGraph Query Language
  *
  * - This library is for defining struct and helper functions that will be used
- *   in the user-defined functions in "ExprFunctions.hpp". Note that functions
+ *   in the user-defined functions in "tg_ExprFunctions.hpp". Note that functions
  *   defined in this file cannot be directly called from TigerGraph Query scripts.
- *   Please put such functions into "ExprFunctions.hpp" under the same directory
+ *   Please put such functions into "tg_ExprFunctions.hpp" under the same directory
  *   where this file is located.
  *
  * - Please don't remove necessary codes in this file
  *
  * - A backup of this file can be retrieved at
- *     <tigergraph_root_path>/dev_<backup_time>/gdk/gsql/src/QueryUdf/ExprUtil.hpp
+ *     <tigergraph_root_path>/dev_<backup_time>/gdk/gsql/src/QueryUdf/tg_ExprUtil.hpp
  *   after upgrading the system.
  *
  ******************************************************************************/
 
-#ifndef EXPRUTIL_HPP_
-#define EXPRUTIL_HPP_
+#ifndef TG_EXPRUTIL_HPP_
+#define TG_EXPRUTIL_HPP_
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -714,5 +714,4 @@ namespace tg {
  */
 }
 
-
-#endif /* EXPRUTIL_HPP_ */
+#endif /* TG_EXPRUTIL_HPP_ */
