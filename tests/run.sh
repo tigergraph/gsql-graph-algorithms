9c7184641b6c9d28bbc2306a47a1fe377b0476f5
RobRossmiller-TG <165701656+RobRossmiller-TG@users.noreply.github.com>
2024-08-21 09:39:43 -0400

    save

--- tests/run.sh
+++ tests/run.sh
@@ -2,3 +2,4 @@ clear
 python3 test/setup.py &&
   python3 test/baseline/create_baselines.py &&
   pytest test/test_centrality.py #test/test_ml.py
+echo 
--- tests/test/setup.py
+++ tests/test/setup.py
@@ -2,6 +2,7 @@ import json
 import os
 import re
 import time
+from glob import glob
 
 import pyTigerGraph as tg
 from dotenv import load_dotenv
@@ -15,21 +16,26 @@ graph_name = "graph_algorithms_testing"
 pattern = re.compile(r'"name":\s*"tg_.*"')
 
 
-def add_reverse_edge(ds: Datasets):
-    with open(f"{dataset.tmp_dir}/{ds.name}/create_schema.gsql") as f:
-        schema: str = f.read()
-    with open(f"{dataset.tmp_dir}/{ds.name}/create_schema.gsql", "w") as f:
-        schema = schema.replace(
-            "ADD DIRECTED EDGE Cite (from Paper, to Paper, time Int, is_train Bool, is_val Bool);",
-            'ADD DIRECTED EDGE Cite (from Paper, to Paper, time Int, is_train Bool, is_val Bool) WITH REVERSE_EDGE="reverse_Cite";',
-        )
-        f.write(schema)
+# def add_reverse_edge(ds: Datasets):
+#     with open(f"{dataset.tmp_dir}/{ds.name}/create_schema.gsql") as f:
+#         schema: str = f.read()
+#     with open(f"{dataset.tmp_dir}/{ds.name}/create_schema.gsql", "w") as f:
+#         schema = schema.replace(
+#             "ADD DIRECTED EDGE Cite (from Paper, to Paper, time Int, is_train Bool, is_val Bool);",
+#             'ADD DIRECTED EDGE Cite (from Paper, to Paper, time Int, is_train Bool, is_val Bool) WITH REVERSE_EDGE="reverse_Cite";',
+#         )
+#         f.write(schema)
+#
+#
+def get_query_path(q_name):
+    pth = glob(f"../algorithms/**/{q_name}.gsql", recursive=True)
+    return pth[0]
 
 
 if __name__ == "__main__":
-    host_name = os.getenv("HOST_NAME")
-    user_name = os.getenv("USER_NAME")
-    password = os.getenv("PASS")
+    host_name = os.environ["HOST_NAME"]
+    user_name = os.environ["USER_NAME"]
+    password = os.environ["PASS"]
     conn = tg.TigerGraphConnection(
         host=host_name,
         username=user_name,
@@ -42,25 +48,29 @@ if __name__ == "__main__":
     if res["error"]:
         exit(1)
     # load the data
-    dataset = Datasets("Cora")
-    add_reverse_edge(dataset)
-    conn.ingestDataset(dataset, getToken=True)
+    # dataset = Datasets("Cora")
+    # add_reverse_edge(dataset)
+    # conn.ingestDataset(dataset, getToken=True)
 
-    dataset = Datasets("graph_algorithms_testing")
-    conn.ingestDataset(dataset, getToken=True)
+    # dataset = Datasets("graph_algorithms_testing")
+    # conn.ingestDataset(dataset, getToken=True)
+    conn.getToken()
 
     conn.graphname = graph_name
     # install the queries
-    feat = conn.gds.featurizer()
+    feat = conn.gds.featurizer()  # type: ignore
     installed_queries = util.get_installed_queries(conn)
     algos = json.dumps(feat.algo_dict, indent=1)
     queries = [
         m.split(": ")[1].replace('"', "").strip() for m in pattern.findall(algos)
     ]
-    for q in tqdm(queries, desc="installing GDS queries"):
-        if q not in installed_queries:
-            print(q)
-            feat.installAlgorithm(q)
 
-    for _ in trange(30, desc="Sleeping while data loads"):
-        time.sleep(1)
+    t = tqdm(queries, desc="installing GDS queries")
+    for q in t:
+        t.set_postfix({"query": q})
+        pth = get_query_path(q)
+        if q not in installed_queries:
+            feat.installAlgorithm(q, pth)
+
+    # for _ in trange(30, desc="Sleeping while data loads"):
+        # time.sleep(1)
